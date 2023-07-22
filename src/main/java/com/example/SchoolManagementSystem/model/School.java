5282d16fee5cef72edcc04789dbcc08e6fba89dc
Atul-Bonde <atul.bonde@gmail.com>
2023-07-22 12:02:08 +0530

    School Entity Class Tested And Updated

---
+++ src/main/java/com/example/SchoolManagementSystem/model/School.java
@@ -0,0 +1,37 @@
+package com.example.SchoolManagementSystem.model;
+
+public class School {
+	
+	private int id;
+	
+	private String name;
+	
+	private String Add;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAdd() {
+		return Add;
+	}
+
+	public void setAdd(String add) {
+		Add = add;
+	}
+	
+	
+
+}
