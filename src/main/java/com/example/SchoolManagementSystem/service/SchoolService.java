93b4584aa00b1e4e0662cb2dc746163d3d715327
Atul-Bonde <atul.bonde@gmail.com>
2023-07-23 09:40:29 +0530

    Schoole Reporistory Created

---
+++ src/main/java/com/example/SchoolManagementSystem/service/SchoolService.java
@@ -0,0 +1,18 @@
+package com.example.SchoolManagementSystem.service;
+
+import org.springframework.stereotype.Service;
+
+import com.example.SchoolManagementSystem.model.Teacher;
+
+@Service
+public class SchoolService implements SchoolServiceI {
+
+	
+
+	@Override
+	public Teacher saveteacher(Teacher teacher) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/main/java/com/example/SchoolManagementSystem/service/SchoolServiceI.java
@@ -0,0 +1,9 @@
+package com.example.SchoolManagementSystem.service;
+
+import com.example.SchoolManagementSystem.model.Teacher;
+
+public interface SchoolServiceI {
+	
+	public Teacher saveteacher(Teacher teacher);
+
+}
